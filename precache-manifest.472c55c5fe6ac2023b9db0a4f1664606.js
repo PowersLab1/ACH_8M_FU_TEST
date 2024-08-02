eb48260612d794c3690f1679c539e36e12b75d2f
PowersLab1 <powerslab3@gmail.com>
2024-08-02 18:42:37 -0400

    Initialize gh-pages

---
+++ precache-manifest.472c55c5fe6ac2023b9db0a4f1664606.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308083c4a63824564222bc027ce8ed11",
+    "url": "/ACH_APP_FU/index.html"
+  },
+  {
+    "revision": "729edbe0fe69313da3a8",
+    "url": "/ACH_APP_FU/static/css/main.a051c6a1.chunk.css"
+  },
+  {
+    "revision": "01a62d586bb287e8883b",
+    "url": "/ACH_APP_FU/static/js/2.c438b123.chunk.js"
+  },
+  {
+    "revision": "133bfd299a168ad86d33834377a7ba0f",
+    "url": "/ACH_APP_FU/static/js/2.c438b123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "729edbe0fe69313da3a8",
+    "url": "/ACH_APP_FU/static/js/main.2fd5692e.chunk.js"
+  },
+  {
+    "revision": "1b18951c31e2958d1286",
+    "url": "/ACH_APP_FU/static/js/runtime-main.7f33ef8e.js"
+  },
+  {
+    "revision": "6956dbda0015b26b8daac462ec922ef5",
+    "url": "/ACH_APP_FU/static/media/psych_logo.6956dbda.jpg"
+  },
+  {
+    "revision": "d9fbf22f557b87c921a73ef996721520",
+    "url": "/ACH_APP_FU/static/media/rating_keydown_1.d9fbf22f.png"
+  },
+  {
+    "revision": "6a659dd41fa6c15739f611662bf6d8d1",
+    "url": "/ACH_APP_FU/static/media/rating_keydown_2.6a659dd4.png"
+  },
+  {
+    "revision": "d955b91a2b4ba7dca40ee5ccb82d995b",
+    "url": "/ACH_APP_FU/static/media/rating_keydown_3.d955b91a.png"
+  },
+  {
+    "revision": "c8b7c6860406752a1915e08a432cb320",
+    "url": "/ACH_APP_FU/static/media/rating_keydown_4.c8b7c686.png"
+  },
+  {
+    "revision": "34958048ce563b1140c19e19f6e72410",
+    "url": "/ACH_APP_FU/static/media/rating_keydown_5.34958048.png"
+  }
+]);
